8170261df3420fcea3d1b87f1546dcdaf7cc342b
graupma <graupma@students.mtsd.k12.wi.us>
2024-05-28 08:35:16 -0500

    needs testing

--- Main.java
+++ Main.java
@@ -1,10 +1,50 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        Uno test = new Uno();
-        test.printDeck();
-        //jkdjkald
+
+        //creates an instance of the uno game and prints the rules of uno
+        Uno game = new Uno();
+        System.out.println("Welcome to Uno! The rules are as follows:");
+        game.rules();
+
+        //asks the user if they are ready to begin the game, but starts the game regardless of their answer
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ready to start (y/n)? ");
+        String ready = input.nextLine();
+        if(ready.toLowerCase().equals("n")){
+            System.out.println("Pity!");
+        }
+
+        //begins the actual game
+        System.out.println("Commencing sequence...");
+        game.play();
+
+        //asks the user if they would like to play again, and keeps asking until the option they enter is equal to either "y" or "n"
+        System.out.println("Would you like to play again (y/n)? ");
+        ready = input.nextLine();
+        while(!ready.toLowerCase().equals("y") && !ready.toLowerCase().equals("n")){
+            System.out.println("Invalid option. Would you like to play again (y/n)? ");
+            ready = input.nextLine();
+        }
+
+        //until the user enters "n", the game is played repeatedly
+        while(ready.toLowerCase().equals("y")){
+            //prints out the rules of a new uno instance and begins the game
+            System.out.println("Great! The rules are as follows:");
+            Uno nGame = new Uno();
+            nGame.rules();
+            nGame.play();
+
+            //reasks the user if they would like to play again, and keeps asking until they enter either "y" or "n"
+            System.out.println("Would you like to play again (y/n)? ");
+            ready = input.nextLine();
+            while(!ready.toLowerCase().equals("y") && !ready.toLowerCase().equals("n")){
+                System.out.println("Invalid option. Would you like to play again (y/n)? ");
+                ready = input.nextLine();
+            }
+        }
+        
+        //thanks the user for playing :)
+        System.out.println("Thanks for playing!");
     }
-    
-    
 }
--- Uno.java
+++ Uno.java
@@ -90,7 +90,8 @@ public class Uno {
 
     
     public boolean validPlay(Card c){
-        if(c.getColor().equals(center.getColor()) || c.getNumber() == center.getNumber()){
+        int num = c.getNumber();
+        if(c.getColor().equals(center.getColor()) || num == center.getNumber() || num == 13 || num == 14){
             return true;
         }
         return false;
@@ -106,21 +107,6 @@ public class Uno {
 
     public void changeCenter(Card cent){
         center=cent; 
-    }
-
-    public void changeCurrentPerson(){
-        if(center.getNumber() <= 0){
-            turn = !turn;
-        }
-        else if(center.getNumber() == )
-    }
-
-    public void drawCard(){
-        if(turn){
-            playerHand.add(drawingPile.pop()); 
-        }
-        else
-            compHand.add(drawingPile.pop());
     }
 
     public void changeCurrentPerson(){
@@ -178,7 +164,128 @@ public class Uno {
         }
     }
  
+    public boolean checkWin(){
+       if(playerHand.isEmpty() || compHand.isEmpty()){
+           return true;
+       }
+       return false;
+    }
 
+    public boolean checkValidCard(Card c, ArrayList<Card> a){
+        boolean ret = false;
+        for(int i=0; i<a.size(); i++){
+            if((a.get(i).getColor().equals(c.getColor())) && (a.get(i).getNumber() == (c.getNumber())))
+            ret=true;
+        }
+        return ret;
+    }
+
+    public Card createTempCard(String p){
+        String te=p.substring(0,1).toLowerCase();
+        String col ="";
+    
+
+        if(te.equals("r"))
+            col="red";
+        else if( te.equals("y"))
+            col="yellow";
+        else if( te.equals("g"))
+            col="green";
+        else if( te.equals("b"))
+            col="blue";
+        else if( te.equals("w"))
+            col="white";
+
+
+        int tn= Integer.valueOf(p.substring(1));
+        return new Card(col, tn);
+
+    }
+
+    public boolean checkValColor(String col){
+        if(col.equals("r") || col.equals("y") || col.equals("b") || col.equals("g") || col.equals("w"))
+            return true;
+        else
+            return false; 
+        
+    }
+
+    public void rules(){
+        System.out.println("\tObjective:");
+        System.out.println("\t\tBe the first player to use all of your cards");
+        System.out.println("\tRules:");
+        System.out.println("\t\t1. Both players are dealt 7 cards at the start of the game");
+        System.out.println("\t\t2. After cards have been dealt, a final card will be drawn and will act as both the bottom of the discard pile and as the card to base the first move on");
+        System.out.println("\t\t3. The card played must match either the color or the number of the top card of the discard pile");
+        System.out.println("\t\t4. A wild card may be played as any color and any number");
+        System.out.println("\t\t5. If a skip is played, the next player's turn will be skipped");
+        System.out.println("\t\t6. If a reverse is played, the next player's turn will be skipped");
+        System.out.println("\t\t7. If a +2 is played, the next player must draw 2 cards and their turn will be skipped");
+        System.out.println("\t\t8. If a wild card is played, the current player must choose a new color and their turn is over");
+        System.out.println("\t\t9. If a wild +4 card is played, the current player must choose a new color, the next player must draw 4 cards, and the next player's turn is skipped");
+    }
+    
+    public void play(){  
+        printPlayerHand();
+        System.out.println("The top card is: " + center.toString());
+        Scanner temp = new Scanner(System.in);
+        System.out.println("\tNumbers: 0-9, 10 = skip, 11 = reverse, 12 = +2, w13 = wild, w14 = wild +4");
+        System.out.println("\tColors: red = r, yellow = y, green = g, blue = b, wild = w");
+        System.out.println("What card would you like to play? (ex: r7 or draw)");
+        String p= temp.nextLine();
+        
+        if(p.equalsIgnoreCase("draw")){
+            drawCard();
+        }
+
+        Card played = createTempCard(p); 
+        
+        while(!checkValidCard(played, playerHand)){
+            System.out.println("Error. please enter a valid card.");
+
+            System.out.println("What card would you like to play? (ex: r7 or draw)");
+        String p= temp.nextLine();
+        
+        if(p.equalsIgnoreCase("draw")){
+            drawCard();
+        }
+
+        
+
+        
+
+        played = createTempCard(p); 
+    }
+
+    //now the card is a valid card in the hand
+
+    if(validPlay(played)){
+    //check what type of card it is and deal with it accordingly 
+    if(played.getColor().equals("white")){
+        Scanner myS= new Scanner(System.in);
+        System.out.println("What color would you like to change it to? ");
+        String colTemp= myS.nextLine();
+        while(!checkValColor(colTemp)) {
+            System.out.println("Error. Invalid color.");
+            System.out.println("What color would you like to change it to? ");
+            colTemp= myS.nextLine();
+        }
+        changeColor(colTemp); 
+
+        if(played.getNumber()==14){
+            turn = !turn;
+            for(int i=0; i<4; i++){
+                drawCard();
+            }
+            turn = !turn;
+        }
+    }
+    center= played; 
+    changeCurrentPerson(); 
+
+    }
+
+    }
 
     
 }
